6f16b277c19e2439f2c90e2d8ec0338f5e9ec408
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-07-01 19:01:29 +0530

    - linting changes

--- lib/native_demo_web/components/core_components.swiftui.ex
+++ lib/native_demo_web/components/core_components.swiftui.ex
@@ -56,7 +56,8 @@ defmodule NativeDemoWeb.CoreComponents.SwiftUI do
 
   attr :type, :string,
     default: "TextField",
-    values: ~w(TextFieldLink DatePicker MultiDatePicker Picker SecureField Slider Stepper TextEditor TextField Toggle hidden)
+    values:
+      ~w(TextFieldLink DatePicker MultiDatePicker Picker SecureField Slider Stepper TextEditor TextField Toggle hidden)
 
   attr :field, Phoenix.HTML.FormField,
     doc: "a form field struct retrieved from the form, for example: `@form[:email]`"
@@ -78,8 +79,7 @@ defmodule NativeDemoWeb.CoreComponents.SwiftUI do
     default: "on",
     values: ~w(on off)
 
-  attr :rest, :global,
-    include: ~w(disabled step)
+  attr :rest, :global, include: ~w(disabled step)
 
   slot :inner_block
 
@@ -91,11 +91,22 @@ defmodule NativeDemoWeb.CoreComponents.SwiftUI do
     |> assign_new(:value, fn -> field.value end)
     |> assign(
       :rest,
-      Map.put(assigns.rest, :style, [
-        Map.get(assigns.rest, :style, ""),
-        (if assigns.readonly or Map.get(assigns.rest, :disabled, false), do: "disabled(true)", else: ""),
-        (if assigns.autocomplete == "off", do: "textInputAutocapitalization(.never) autocorrectionDisabled()", else: "")
-      ] |> Enum.join(" "))
+      Map.put(
+        assigns.rest,
+        :style,
+        [
+          Map.get(assigns.rest, :style, ""),
+          if(assigns.readonly or Map.get(assigns.rest, :disabled, false),
+            do: "disabled(true)",
+            else: ""
+          ),
+          if(assigns.autocomplete == "off",
+            do: "textInputAutocapitalization(.never) autocorrectionDisabled()",
+            else: ""
+          )
+        ]
+        |> Enum.join(" ")
+      )
     )
     |> input()
   end
@@ -256,6 +267,7 @@ defmodule NativeDemoWeb.CoreComponents.SwiftUI do
     <VStack style={[
       "navigationTitle(:title)",
       "navigationSubtitle(:subtitle)",
+      "navigationBarTitleDisplayMode(.automatic)",
       "toolbar(content: :toolbar)"
     ]}>
       <Text template="title">
@@ -425,7 +437,7 @@ defmodule NativeDemoWeb.CoreComponents.SwiftUI do
 
   slot :inner_block, required: true
 
-  def button(%{ type: "submit" } = assigns) do
+  def button(%{type: "submit"} = assigns) do
     ~LVN"""
     <Section>
       <LiveSubmitButton style={[
@@ -558,7 +570,9 @@ defmodule NativeDemoWeb.CoreComponents.SwiftUI do
 
   attr :url, :string, required: true
   attr :rest, :global
-  slot :empty, doc: """
+
+  slot :empty,
+    doc: """
     The empty state that will render before has successfully been downloaded.
 
         <.image url={~p"/assets/images/logo.png"}>
@@ -569,7 +583,9 @@ defmodule NativeDemoWeb.CoreComponents.SwiftUI do
 
     [See SwiftUI docs](https://developer.apple.com/documentation/swiftui/asyncimagephase/success(_:))
     """
-  slot :success, doc: """
+
+  slot :success,
+    doc: """
     The success state that will render when the image has successfully been downloaded.
 
         <.image url={~p"/assets/images/logo.png"}>
@@ -577,22 +593,22 @@ defmodule NativeDemoWeb.CoreComponents.SwiftUI do
         </.image>
 
     [See SwiftUI docs](https://developer.apple.com/documentation/swiftui/asyncimagephase/success(_:))
-    """
-  do
+    """ do
     attr :class, :string
     attr :style, :string
   end
-  slot :failure, doc: """
-    The failure state that will render when the image fails to downloaded.
 
-        <.image url={~p"/assets/images/logo.png"}>
-          <:failure class="image-fail"/>
-        </.image>
+  slot :failure,
+    doc: """
+      The failure state that will render when the image fails to downloaded.
 
-    [See SwiftUI docs](https://developer.apple.com/documentation/swiftui/asyncimagephase/failure(_:))
+          <.image url={~p"/assets/images/logo.png"}>
+            <:failure class="image-fail"/>
+          </.image>
 
-  """
-  do
+      [See SwiftUI docs](https://developer.apple.com/documentation/swiftui/asyncimagephase/failure(_:))
+
+    """ do
     attr :class, :string
     attr :style, :string
   end
@@ -609,7 +625,7 @@ defmodule NativeDemoWeb.CoreComponents.SwiftUI do
     """
   end
 
-  defp image_success(%{ slot: [%{ inner_block: nil }] } = assigns) do
+  defp image_success(%{slot: [%{inner_block: nil}]} = assigns) do
     ~LVN"""
     <AsyncImage image template="phase.success" :for={slot <- @slot} class={Map.get(slot, :class)} {%{ style: Map.get(slot, :style) }} />
     """
@@ -623,7 +639,7 @@ defmodule NativeDemoWeb.CoreComponents.SwiftUI do
     """
   end
 
-  defp image_failure(%{ slot: [%{ inner_block: nil }] } = assigns) do
+  defp image_failure(%{slot: [%{inner_block: nil}]} = assigns) do
     ~LVN"""
     <AsyncImage error template="phase.failure" :for={slot <- @slot} class={Map.get(slot, :class)} {%{ style: Map.get(slot, :style) }} />
     """
